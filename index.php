2b6ae935784039ca912a61c18459b0362f6503c2
chirag_sun <chirag.kalsariya@thesunflowerlab.com>
2021-10-13 18:05:34 +0530

    change app link

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ __ez.queue.addFunc("attach_ezolpl", "attach_ezolpl", ["3ad23945-23c6-4564-7ad7-d
 </ul>
 <p>Improve your image by including an image. </p>
 <p><img src="http://www.mygifs.com/CoverImage.gif" alt="A Great HTML Resource"/></p>
-<p>Add a link to your favorite <a href="turk://breakersparadise/stripe/true">Web site</a>.
+<p>Add a link to your favorite <a href="turk://turk/stripe/true">Web site</a>.
 Break up your page with a horizontal rule or two. </p>
 <hr/>
 <p>Finally, link to <a href="app://open.my.app/lorem/ipsum">another page</a> in your own Web site.</p>
